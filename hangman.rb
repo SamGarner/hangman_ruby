9e4dd3d85d4c6529fa5d93d294dd4e5f1fbe5842
SamGarner <samuel.w.garner@gmail.com>
2020-10-15 15:15:56 -0500

    create properly sized Board array and add secret word to it: Board initialize, secret_word_letters, and add_secret_word_to_board

--- hangman.rb
+++ hangman.rb
@@ -23,6 +23,7 @@ class Game
     @remaining_guess_counter = 6
     create_dictionary
     get_random_secret_word
+    @board = Board.new(self.secret_word)
     binding.pry
   end
 
@@ -33,11 +34,41 @@ class Game
   def get_random_secret_word # add sample to File.read dictionary method to condense
     @secret_word = dictionary.sample
   end
+
+  private
+
+  attr_reader :secret_word
 end
 
+# Board comment
 class Board
-  def initialize
+  # call @board to see all the variables or use attr_reader to call var directly
+
+  def initialize(secret_word)
+    # @length = secret_word.length
+    @game_board = Array.new(2) { Array.new(secret_word.length) }
+    secret_word_letters(secret_word)
+    add_secret_word_to_board
   end
+
+  def secret_word_letters(secret_word)
+    @letters = secret_word.split('')
+  end
+
+  def add_secret_word_to_board
+    # below version does not work if a letter repeats in the word. less straightforward as well
+    # letters.each do |letter|
+    #   game_board[0][letters.index(letter)] = letter
+    # end
+    (0..letters.length - 1).each do |index|
+      game_board[0][index] = letters[index]
+    end
+  end
+
+  private
+
+  attr_reader :letters
+  attr_accessor :game_board
 end
 
 current_game = Game.new
