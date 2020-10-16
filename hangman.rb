f2ef264a55dc6bcf71820e72200010ec1b727a16
SamGarner <samuel.w.garner@gmail.com>
2020-10-16 14:29:04 -0500

    game working, code ready for cleanup

--- hangman.rb
+++ hangman.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: false
+
 require 'pry'
 
 # Load a word between 5-12 letters long as the secret word (Game)
@@ -14,7 +15,7 @@ require 'pry'
 
 # Load a word between 5-12 letters long as the secret word (Game)
 class Game
-  attr_reader :dictionary
+  attr_reader :dictionary, :guess
 
   DICTIONARY_REGEX = /\r\n([a-zA-Z]{5,12})\r\n/.freeze
   DICTIONARY_FILE = 'dictionary.txt'.freeze
@@ -24,7 +25,40 @@ class Game
     create_dictionary
     get_random_secret_word
     @board = Board.new(self.secret_word)
-    binding.pry
+    #binding.pry
+  end
+
+  def play
+    while @remaining_guess_counter > 0
+      puts @board.gameboard[0].join() #troubleshooting only
+      # puts @board.incorrect_guesses.inspect
+      puts @board.gameboard[1].join()
+      # puts remaining_guess_counter.inspect
+      # puts @remaining_guess_counter.inspect
+      puts " #{remaining_guess_counter} incorrect guesses remaining."
+      puts "incorrect guesses: #{@board.incorrect_guesses.join('-')}"
+      get_player_guess
+      # puts 'guess inspect'
+      # puts guess.inspect
+      if guess == '-'
+        puts 'Invalid input. Please choose a valid letter.'
+        next
+      else
+      # next unless guess != '-'
+        @board.check_guess(guess)
+      end
+      self.remaining_guess_counter = 6 - @board.incorrect_guesses.uniq.length 
+        # THE IMPORTANCE OF SELF HERE SO PROGRAM DOESN'T THINK MAKING A QUICK LOCAL VARIABLE
+      # p remaining_guess_counter
+      # p @remaining_guess_counter
+      # binding.pry
+      show_board_status
+      # game_won? ? (puts "Congrats, you've guessed #{@board.gameboard[0].join()} correctly!") : next
+      # game_won? ? (puts "Congrats, you've guessed the word!") : nil
+      # game_lost?
+      exit if game_won? || game_lost?
+      # binding.pry
+    end
   end
 
   def create_dictionary
@@ -37,12 +71,40 @@ class Game
 
   def get_player_guess
     puts 'Guess a letter that has not been chosen yet:'
-    @guess = gets.chomp.downcase
+    @input = gets.chomp.downcase
+    # puts 'input inspect:'
+    # puts @input.inspect
+    @guess = input.match(/[a-z]/) && input.length == 1 ? input : '-'
+    #binding.pry
+  end
+
+  def show_board_status
+    puts @board.gameboard[1].join(' ')
+  end
+
+  def game_won?
+    if @board.gameboard[1].all? { |n| n.match /[a-z]/ }
+      puts "Congrats, you've guessed the word!"
+      true
+    else
+      false
+    end
+  end
+
+  def game_lost?
+    # puts "not working"
+    # puts remaining_guess_counter
+    # p remaining_guess_counter.zero?
+    if remaining_guess_counter.zero?
+      puts "Sorry, you're out of guesses. The word was #{@board.gameboard[0].join('-')}"
+    else false
+    end
   end
 
   private
 
-  attr_reader :secret_word
+  attr_reader :secret_word, :input
+  attr_accessor :remaining_guess_counter
 end
 
 # Board comment
@@ -55,6 +117,8 @@ class Board
     secret_word_letters(secret_word)
     add_secret_word_to_board
     self.gameboard[1].map! { '_' } # set guess/display row to show 'blank spaces'
+    # change line above to have the @ so more obvious it's same as above?
+    @incorrect_guesses = []
   end
 
   def secret_word_letters(secret_word)
@@ -72,19 +136,31 @@ class Board
   end
 
   def check_guess(letter)
+    @indices = gameboard[0].each_index.select { |i| gameboard[0][i] == letter }
+      if @indices.empty?
+        incorrect_guesses << letter
+        puts 'Sorry, the secret word does not contain that letter'
+      else 
+        @indices.each do |index|
+          gameboard[1][index] = letter
+        end
+        puts 'Good guess!'
+      end
+    # binding.pry
   end
 
   # comparing input against secret word - count to get the number of occurences then loop using .index/each_index to replace?
   # e.g. arr = ['x', 'o', 'x', '.', '.', 'o', 'x']
     #  p arr.each_index.select{|i| arr[i] == 'x'} # =>[0, 2, 6]
 
-  private
+  #private
 
-  attr_reader :letters
-  attr_accessor :gameboard
+  attr_reader :letters, :indices
+  attr_accessor :gameboard, :incorrect_guesses
 end
 
 current_game = Game.new
+current_game.play
 # set remaining guesses = 6 (Game)
 # An array of two arrays of the same length: secret word and displayed word (Board)
 # loop:
@@ -101,3 +177,4 @@ current_game = Game.new
     # def get_random_secret_word
     #   @secret_word = dictionary.sample
     # end
+  # priave vs non-private attr_ getter/setter methods
